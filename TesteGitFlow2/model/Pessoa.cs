cf0effa81ba3395609bcd6e9cda3929f625b588d
Edson <edsonkazyamamoto@gmail.com>
2019-11-06 11:41:22 -0300

    Adicionado metodo de login

--- TesteGitFlow2/model/Pessoa.cs
+++ TesteGitFlow2/model/Pessoa.cs
@@ -5,8 +5,16 @@ public class Pessoa
     public DateTime dataNascimento;
     public string nome;
     public string rg;
+    public string email;
+    public string password;
 
-    public Pessoa()
-	{
-	}
+    public static Pessoa Login(string email, string password) {
+        return LoginPessoa(email, password);
+    }
+
+    public static Pessoa LoginPessoa(string email, string password){
+        if( email == "john" && password == "doe")
+        Pessoa p = new Pessoa();
+        return p;
+    }
 }
